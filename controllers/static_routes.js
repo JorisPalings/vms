6c2b0dc42dcd9a58c6e5c7da0ceb24507ea4080e
Bert Vandormael <Bert Vandormael>
2017-02-14 16:14:59 +0100

    [FEATURE] Changed file static_routes.js

--- controllers/static_routes.js
+++ controllers/static_routes.js
@@ -1,3 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, '../layouts/landing.html'));
+});
+
+router.use(express.static(path.join(__dirname, '/../assets')));
+router.use(express.static(path.join(__dirname, '/../templates')));
+
+module.exports = router;
