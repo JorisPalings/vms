40e9e61d55b7860f6b7f7653d09ae5034c4c093b
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-14 17:56:50 +0100

    [FEATURE] Added the model file for the persons (employee or external)

---
+++ models/persons.js
@@ -0,0 +1,35 @@
+var db = require('../db');
+var mongoose = require('mongoose');
+
+var options = { discriminatorKey: 'kind' };
+
+var person = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    pictureURL: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    }
+}, options);
+
+var employee = new mongoose.Schema({
+    hashedPassword: {
+        type: String,
+        required: true
+    }
+}, options);
+
+var external = new mongoose.Schema({
+    company: {type: [mongoose.Schema.Types.ObjectId], ref: 'Company'}
+}, options);
+
+mongoose.model('Person', person);
+//discriminator is used for stating that a class is a subclass of another class
+Person.discriminator('Employee', employee);
+Person.discriminator('External', external);
