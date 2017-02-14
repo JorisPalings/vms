062818aa4ee84a007dacfed627497b699fc4f18c
Bert Vandormael <Bert Vandormael>
2017-02-14 16:32:16 +0100

    [FEATURE] File api/routes.js created + changes

--- controllers/static_routes.js
+++ controllers/static_routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../layouts/landing.html'));
