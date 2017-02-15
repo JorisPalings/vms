d6b3a0aaec839fd6a93c7cab8e08c3e3668106c6
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-15 19:33:13 +0100

    [PATCH] Added the test URI to the database

--- db.js
+++ db.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
-var dbUri = 'mongodb://178.62.206.7:27017/vms';
+var dbUri = 'mongodb://127.0.0.1:27017/vms';
+
+if(process.env.NODE_ENV === 'production'){
+  dbUri = 'mongodb://178.62.206.7:27017/vms'
+}
 
 mongoose.connect(dbUri, function(){
   console.log('Connected to MongoDB');
