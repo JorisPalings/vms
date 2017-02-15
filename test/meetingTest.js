cde64d71552a2432f520ce9b09fa89401fea5276
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-15 20:56:09 +0100

    [PATCH] Added the external feature to the test file

--- test/meetingTest.js
+++ test/meetingTest.js
@@ -4,16 +4,24 @@ const expect = require('chai').expect;
 const Meeting = require('../models/meeting');
 const Note = require('../models/note');
 const Employee = require('../models/persons');
+const External = require('../models/persons');
 const Project = require('../models/project');
+const Company = require('../models/company');
+const Location = require('../models/location');
 
 describe('Meeting', function() {
     it('should be invalid if room is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
+        let external = new External({name:"Nick Van Vynckt", pictureURL:"url", email:"e-mail", company: new Company({name: 'Craftworkz',contactPerson: new External({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", company:this}),
+            location: new Location({
+                address:"Gaston Geenslaan 11 B4", postalCode:3000, country:"BEL"
+            })})});
         let meeting = undefined;
         meeting = new Meeting({
            date: Date.now,
            notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            meetee: [employee],
+           external: [external],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
  
@@ -25,11 +33,16 @@ describe('Meeting', function() {
 
     it('should be invalid if date is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
+        let external = new External({name:"Nick Van Vynckt", pictureURL:"url", email:"e-mail", company: new Company({name: 'Craftworkz',contactPerson: new External({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", company:this}),
+            location: new Location({
+                address:"Gaston Geenslaan 11 B4", postalCode:3000, country:"BEL"
+            })})});
         let meeting = undefined;
         meeting = new Meeting({
            room: "Cho Oyu",
            notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            meetee: [employee],
+           external: [external],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
  
@@ -41,11 +54,16 @@ describe('Meeting', function() {
 
     it('should be invalid if notes is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
+        let external = new External({name:"Nick Van Vynckt", pictureURL:"url", email:"e-mail", company: new Company({name: 'Craftworkz',contactPerson: new External({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", company:this}),
+            location: new Location({
+                address:"Gaston Geenslaan 11 B4", postalCode:3000, country:"BEL"
+            })})});
         let meeting = undefined;
         meeting = new Meeting({
            room: "Cho Oyu",
            date: Date.now,
            meetee: [employee],
+           external: [external],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
  
@@ -57,10 +75,15 @@ describe('Meeting', function() {
 
     it('should be invalid if metee is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
+        let external = new External({name:"Nick Van Vynckt", pictureURL:"url", email:"e-mail", company: new Company({name: 'Craftworkz',contactPerson: new External({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", company:this}),
+            location: new Location({
+                address:"Gaston Geenslaan 11 B4", postalCode:3000, country:"BEL"
+            })})});
         let meeting = undefined;
         meeting = new Meeting({
            room: "Cho Oyu",
            date: Date.now,
+           external: [external],
            notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
@@ -73,11 +96,16 @@ describe('Meeting', function() {
 
     it('should be invalid if project is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
+        let external = new External({name:"Nick Van Vynckt", pictureURL:"url", email:"e-mail", company: new Company({name: 'Craftworkz',contactPerson: new External({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", company:this}),
+            location: new Location({
+                address:"Gaston Geenslaan 11 B4", postalCode:3000, country:"BEL"
+            })})});
         let meeting = undefined;
         meeting = new Meeting({
            room: "Cho Oyu",
            date: Date.now,
            meetee: [employee],
+           external: [external],
            notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
         });
  
@@ -87,13 +115,35 @@ describe('Meeting', function() {
         });
     });
 
-    it('should be valid when everything is complete', function(){
+    it('should be invalid if external is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
         let meeting = undefined;
         meeting = new Meeting({
            room: "Cho Oyu",
            date: Date.now,
            meetee: [employee],
+           notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
+           project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
+        });
+ 
+        meeting.validate(function(err) {
+            expect(err.errors.external).to.exist;
+            done();
+        });
+    });
+
+    it('should be valid when everything is complete', function(){
+        let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
+        let external = new External({name:"Nick Van Vynckt", pictureURL:"url", email:"e-mail", company: new Company({name: 'Craftworkz',contactPerson: new External({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", company:this}),
+            location: new Location({
+                address:"Gaston Geenslaan 11 B4", postalCode:3000, country:"BEL"
+            })})});
+        let meeting = undefined;
+        meeting = new Meeting({
+           room: "Cho Oyu",
+           date: Date.now,
+           meetee: [employee],
+           external: [external],
            notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
