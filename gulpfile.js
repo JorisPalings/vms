a59a7b6f4064e08d8b4184684badcce3e9573a1d
JorisPalings <joris.palings@student.ucll.be>
2017-02-15 12:05:28 +0100

    [PATCH] Whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,17 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const imagemin = require('gulp-imagemin');
+const git = require('gulp-git');
+const run = require('gulp-run');
 
 gulp.task('sass', function() {
- return gulp.src('./sass/**/*.scss')
-   .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-   .pipe(gulp.dest('./assets/css'));
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./assets/css'));
 });
 
 gulp.task('images', function() {
- return gulp.src('./images/**/*', {base: '.'})
-   .pipe(imagemin())
-   .pipe(gulp.dest('./assets'))
+    return gulp.src('./images/**/*', {base: '.'})
+        .pipe(imagemin())
+        .pipe(gulp.dest('./assets'))
 });
