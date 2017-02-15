71ca023e2c97231284ec505c7663fcecbc75bfe8
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-15 20:55:21 +0100

    [UPDATE] Meeting did not have external yet

--- models/meeting.js
+++ models/meeting.js
@@ -21,10 +21,15 @@ let meeting = new mongoose.Schema({
         required:true,
         validate: [arrayLimit, '{PATH} cannot be 0']
     },
+    external: {
+        type:[{type: [mongoose.Schema.Types.ObjectId], ref: 'External'}],
+        required:true,
+        validate: [arrayLimit, '{PATH} cannot be 0']
+    },
     project: {type: [mongoose.Schema.Types.ObjectId], ref: 'Project', required:true}
 });
 
-meeting.index({room: 1, date: 1, notes: 1, meetee: 1, project: 1}, {unique: true});
+meeting.index({room: 1, date: 1, notes: 1, meetee: 1, external: 1, project: 1}, {unique: true});
 
 module.exports = mongoose.model('Meeting', meeting);
 
