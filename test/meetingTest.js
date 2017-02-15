2a42a8e2ff49c4b2d541ebde6ff08baf96b3bebb
Bert Vandormael <bert.vandormael@gmail.com>
2017-02-15 14:19:00 +0100

    [PATCH] meetingTest.js - the working version

--- test/meetingTest.js
+++ test/meetingTest.js
@@ -9,9 +9,10 @@ const Project = require('../models/project');
 describe('Meeting', function() {
     it('should be invalid if room is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
-        let meeting = new Meeting({
-           date: Date.now(),
-           notes: [new Note({content:"This is a note!", date:Date.now(), meeting:meeting, author:employee})],
+        let meeting = undefined;
+        meeting = new Meeting({
+           date: Date.now,
+           notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            meetee: [employee],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
@@ -24,9 +25,10 @@ describe('Meeting', function() {
 
     it('should be invalid if date is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
-        let meeting = new Meeting({
+        let meeting = undefined;
+        meeting = new Meeting({
            room: "Cho Oyu",
-           notes: [new Note({content:"This is a note!", date:Date.now(), meeting:meeting, author:employee})],
+           notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            meetee: [employee],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
@@ -39,9 +41,10 @@ describe('Meeting', function() {
 
     it('should be invalid if notes is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
-        let meeting = new Meeting({
+        let meeting = undefined;
+        meeting = new Meeting({
            room: "Cho Oyu",
-           date: Date.now(),
+           date: Date.now,
            meetee: [employee],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
@@ -54,10 +57,11 @@ describe('Meeting', function() {
 
     it('should be invalid if metee is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
-        let meeting = new Meeting({
+        let meeting = undefined;
+        meeting = new Meeting({
            room: "Cho Oyu",
-           date: Date.now(),
-           notes: [new Note({content:"This is a note!", date:Date.now(), meeting:meeting, author:employee})],
+           date: Date.now,
+           notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
            project: new Project({name:"3D printing", budget:300, meetings:[meeting]})
         });
  
@@ -69,11 +73,12 @@ describe('Meeting', function() {
 
     it('should be invalid if project is empty', function(done) {
         let employee = new Employee({name:"Georges Petrofski", pictureURL:"url", email:"e-mail", hashedPassword:"imahacker"});
-        let meeting = new Meeting({
+        let meeting = undefined;
+        meeting = new Meeting({
            room: "Cho Oyu",
-           date: Date.now(),
+           date: Date.now,
            meetee: [employee],
-           notes: [new Note({content:"This is a note!", date:Date.now(), meeting:meeting, author:employee})],
+           notes: [new Note({content:"This is a note!", date:Date.now, meeting:meeting, author:employee})],
         });
  
         meeting.validate(function(err) {
