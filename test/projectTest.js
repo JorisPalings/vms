29503a0d640fc8c11a7290709b37dbc0edded80f
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-15 09:49:08 +0100

    [PATCH] Added mongoose as dependency

--- test/projectTest.js
+++ test/projectTest.js
@@ -1,9 +1,9 @@
 var expect = require('chai').expect;
-var Project = require('../../models/Project');
+var Project = require('../models/Project');
 
-describe('Product', function(){
+describe('Project', function(){
     it('should be invalid if name is empty', function(done){
-        var product = new product();
+        var product = new Project();
 
         product.validate(function(err){
             expect(err.errors.name).to.exist;
