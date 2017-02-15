74d785e20ea656e41e59b71c452f432ab04ffaed
Nick Van Vynckt <nick.vanvynckt@hotmail.com>
2017-02-15 11:39:02 +0100

    [FEATURE] Finished the location test file and the project test file

---
+++ test/locationTest.js
@@ -0,0 +1,59 @@
+'use strict'
+
+const expect = require('chai').expect;
+const Location = require('../models/location');
+
+describe('Location', function(){
+    it('should be invalid if address is empty', function(done){
+        let locationEmptyAddress = new Location({
+            postalCode: 3150,
+            country: "BEL"
+        });
+        locationEmptyAddress.validate(function(err){
+            expect(err.errors.address).to.exist;
+            done();
+        });
+    });
+    it('should be invalid if postalCode is empty', function(done){
+        let locationEmptyPostalCode = new Location({
+            address: "Haachtsestraatje 9",
+            country: "BEL"
+        });
+        locationEmptyPostalCode.validate(function(err){
+            expect(err.errors.postalCode).to.exist;
+            done();
+        });
+    });
+    it('should be invalid if country is empty', function(done){
+        let locationEmptyCountry = new Location({
+            address: "Haachtsestraatje 9",
+            postalCode: 3150
+        });
+        locationEmptyCountry.validate(function(err){
+            expect(err.errors.country).to.exist;
+            done();
+        });
+    });
+    it('should be invalid if country field is too long', function(done){
+        let locationCountryFieldTooLong = new Location({
+            address: "Haachtsestraatje 9",
+            postalCode: 3150,
+            country: "BELGIUM"
+        });
+        locationCountryFieldTooLong.validate(function(err){
+            expect(err.errors.country).to.exist;
+            done();
+        });
+    });
+    it('should be valid if everything is complete', function(done){
+        let locationComplete = new Location({
+            address: "Haachtsestraatje 9",
+            postalCode: 3150,
+            country: "BEL"
+        });
+        locationComplete.validate(function(err){
+            expect(err).to.not.exist;
+            done();
+        });
+    });
+});
--- test/projectTest.js
+++ test/projectTest.js
@@ -1,14 +1,41 @@
 var expect = require('chai').expect;
 var Project = require('../models/project');
+var Meeting = require('../models/meeting');
 
 describe('Project', function(){
     it('should be invalid if name is empty', function(done){
-        var project = new Project({
-            
+        var projectEmptyName = new Project({
+            budget: 500
         });
-        project.validate(function(err){
+        projectEmptyName.validate(function(err){
             expect(err.errors.name).to.exist;
             done();
         });
+    });
+    it('should be invalid if budget is empty', function(done){
+        var projectEmptyBudget = new Project({
+            name: "VMS"
+        });
+        projectEmptyBudget.validate(function(err){
+            expect(err.errors.budget).to.exist;
+            done();
+        });
+    });
+    it('should have an array of meetings', function(done){
+        var completeProject = new Project({
+            name: "VMS",
+            budget: 500,
+            meetings: [new Meeting({
+                room: "Everest",
+                date: Date.now
+            }), new Meeting({
+                room: "Kilimanjaro",
+                date: Date.now
+            })]
+        });
+        completeProject.validate(function(err){
+            expect(err).to.not.exist;
+            done();
+        })
     });
 });
