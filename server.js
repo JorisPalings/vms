33d838941f73fb6bf3bde8be8d2326470d8c406e
sanderlenaerts <sander.lenaerts@gmail.com>
2017-02-20 13:20:02 +0100

    [PATCH] Removed unused imports and fixed rename

--- server.js
+++ server.js
@@ -4,8 +4,6 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var express_validator = require('express-validator');
 
 var port = 3000;
 
@@ -18,8 +16,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(require('./controllers/static_routes'));
-app.use('/api', require('./controllers/api/routes'));
+app.use(require('./routes/static_routes'));
+app.use('/api', require('./routes/api/routes'));
 
 app.listen(port, function(){
     console.log('Server started on port ' + port);
