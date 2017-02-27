c6d3f748a338d7d49cd4d9032709f1647020ef11
sanderlenaerts <sander.lenaerts@gmail.com>
2017-02-27 12:33:09 +0100

    [FEATURE] Email validator added

---
+++ client/app/directives/mail-validator.ts
@@ -0,0 +1,16 @@
+import {FormControl} from '@angular/forms';
+
+
+export class EmailValidator {
+
+   static isValidMailFormat(control: FormControl){
+        let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
+
+        if (!EMAIL_REGEXP.test(control.value)){
+            return { "invalidEmailAddress": true };
+        }
+
+        return null;
+    }
+
+}
