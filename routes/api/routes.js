5387fcbab3958e32e80b6a3cbffe02b99b602039
sanderlenaerts <sander.lenaerts@gmail.com>
2017-02-28 10:04:15 +0100

    [FEATURE] Added routing link for google calendars

--- routes/api/routes.js
+++ routes/api/routes.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var auth = require('./authentication');
+var user = require('./user');
 
 // Authentication
 // 1. Login
@@ -9,5 +10,9 @@ router.post('/login', auth.login);
 
 // 2. Register
 router.post('/register', auth.register);
+
+// User data
+// 1. Get google calendars
+router.post('/google-calendars', user.googlecalendars);
 
 module.exports = router;
