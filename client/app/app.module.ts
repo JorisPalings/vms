ecddfed3f4a1b87c72c9b866f3327ec5c9442e28
JorisPalings <joris.palings@student.ucll.be>
2017-03-01 16:14:41 +0100

    [FEATURE] Added meeting participant component and increased global line-height

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -29,6 +29,7 @@ import { SettingsComponent } from './settings/settings.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './landing/login.component';
 import { MeetingDetailsComponent } from './meeting-details/meeting-details.component';
+import { ParticipantComponent } from './meeting-details/participant.component';
 import { TimelineComponent } from './shared/timeline.component';
 import { MeetingComponent } from './shared/meeting.component';
 import { DropdownComponent } from './shared/dropdown.component';
@@ -60,6 +61,7 @@ import { IntegrationButtonsComponent } from './shared/integration-buttons.compon
     BrandingComponent,
     MeetingComponent,
     MeetingDetailsComponent,
+    ParticipantComponent,
     TimelineComponent,
     DropdownComponent,
     ProfileComponent,
--- client/app/meeting-details/meeting-details.component.ts
+++ client/app/meeting-details/meeting-details.component.ts
@@ -15,7 +15,19 @@ import { Component } from '@angular/core';
         <h3>Today 10:00 - 12:00</h3>
         <h3>Everest</h3>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quasi sunt ipsum magni, doloribus commodi, quas, iure soluta possimus illo dignissimos. Perferendis autem corrupti, minima quidem expedita natus qui assumenda.</p>
-        <button><i class="fa fa-pencil-square"></i> View previous notes</button>
+        <div class="one-half column offset-by-three">
+          <form>
+            <button><i class="fa fa-pencil-square-o"></i> View previous notes</button>
+          </form>
+        </div>
+        <div class="row">
+          <participant class="one-half column"></participant>
+          <participant class="one-half column"></participant>
+        </div>
+        <div class="row">
+          <participant class="one-half column"></participant>
+          <participant class="one-half column"></participant>
+        </div>
       </div>
     </div>
   </main>
