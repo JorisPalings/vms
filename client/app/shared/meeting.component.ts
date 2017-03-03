b28fb8765915fe0c7d32d77a82f03af5ff25ac5f
JorisPalings <joris.palings@student.ucll.be>
2017-03-03 14:23:26 +0100

    [PATCH] Removed routerLink from bell, fixed whitespace

--- client/app/shared/meeting.component.ts
+++ client/app/shared/meeting.component.ts
@@ -4,28 +4,28 @@ import { Meeting } from '../_models/meeting';
 @Component({
   selector: 'meeting',
   template: `
-  <div class="card-wrapper" [routerLink]="['/meeting', meeting.id]">
-    <div class="meeting-card">
+  <div class="card-wrapper">
+    <div class="meeting-card" [routerLink]="['/meeting', meeting.id]">
       <span class="time-box">
-            <div class="time-box-container">
-              <span class="start-time">{{meeting.start | date:'HH:mm'}}</span>
-      <span> - </span>
-      <span class="end-time">{{meeting.end | date:'HH:mm'}}</span>
+        <div class="time-box-container">
+          <span class="start-time">{{meeting.start | date:'HH:mm'}}</span>
+          <span> - </span>
+          <span class="end-time">{{meeting.end | date:'HH:mm'}}</span>
+        </div>
+      </span>
+      <span class="meeting-info">
+        <p><span *ngFor="let external of meeting.externals.length === 0 ? meeting.meetees : meeting.externals; let isLast=last">{{external.fname}} {{external.lname}}{{isLast ? '' : ', '}}</span></p>
+        <p>{{meeting.summary}}</p>
+        <p>{{meeting.room === undefined ? 'No room has been set for this meeting' : meeting.room}}</p>
+      </span>
+      <span class="profile-image">
+        <img src="{{meeting.externals[0] === undefined ? (meeting.meetees[0] === undefined ? 'https://cdn-images-1.medium.com/max/800/0*9BYGa20RkF4rTpJU.png' : meeting.meetees[0].pictureURL ) : meeting.externals[0].pictureURL}}">
+      </span>
     </div>
-    </span>
-    <span class="meeting-info">
-            <p><span *ngFor="let external of meeting.externals.length === 0 ? meeting.meetees : meeting.externals; let isLast=last">{{external.fname}} {{external.lname}}{{isLast ? '' : ', '}}</span></p>
-            <p>{{meeting.summary}}</p>
-            <p>{{meeting.room === undefined ? 'No room has been set for this meeting' : meeting.room}}</p>
-          </span>
-    <span class="profile-image">
-            <img src="{{meeting.externals[0] === undefined ? (meeting.meetees[0] === undefined ? 'https://cdn-images-1.medium.com/max/800/0*9BYGa20RkF4rTpJU.png' : meeting.meetees[0].pictureURL ) : meeting.externals[0].pictureURL}}">
-          </span>
+    <div class="bell">
+      <span>Running late</span>
+    </div>
   </div>
-  <div class="bell">
-    <span>Running late</span>
-  </div>
-</div>
   `,
   styleUrls: ['../dist/assets/css/meeting.css']
 })
