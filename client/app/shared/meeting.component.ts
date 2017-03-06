8c9e3db54830c7721244009b304ee13c108d5f20
Bert Vandormael <bert.vandormael@gmail.com>
2017-03-06 15:37:30 +0100

    [PATCH] 'Just you' is shown when creator is only meetee

--- client/app/shared/meeting.component.ts
+++ client/app/shared/meeting.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Meeting } from '../_models/meeting';
+import { AuthenticationService } from '../shared/services/authentication.service';
 
 @Component({
   selector: 'meeting',
@@ -16,13 +17,13 @@ import { Meeting } from '../_models/meeting';
       <span class="meeting-info">
         <p>
           <!-- Don't add line breaks in the HTML between these spans. It will cause a space to be rendered -->
-          <i class="fa fa-fw fa-users"></i><span *ngIf="meeting.externals != undefined || meeting.meetees != undefined"><span *ngFor="let external of meeting.externals.length === 0 ? meeting.meetees : meeting.externals; let isLast=last">{{external.fname}} {{external.lname}}{{isLast ? '' : ', '}}</span></span><span *ngIf="(meeting.externals == undefined || meeting.externals[0] === undefined) && (meeting.meetees == undefined || meeting.meetees[0] === undefined)">Just you</span>
+          <i class="fa fa-fw fa-users"></i><span *ngIf="(meeting.externals != undefined && meeting.externals.length > 0)  || (meeting.meetees != undefined && meeting.meetees.length > 0 && meeting.meetees[0].id != employeeId)"><span *ngFor="let external of meeting.externals.length === 0 ? meeting.meetees : meeting.externals; let isLast=last">{{external.fname}} {{external.lname}}{{isLast ? '' : ', '}}</span></span><span *ngIf="(meeting.externals == undefined || meeting.externals[0] === undefined) && (meeting.meetees == undefined || meeting.meetees[0] === undefined || meeting.meetees[0].id === employeeId)">Just you</span>
         </p>
         <p><i class="fa fa-fw fa-folder"></i>{{meeting.summary}}</p>
         <p><i class="fa fa-fw fa-map-marker"></i>{{meeting.room === undefined ? 'No room has been set for this meeting' : meeting.room}}</p>
       </span>
       <span class="profile-image">
-        <img src="{{ meeting.externals === undefined || meeting.externals[0] === undefined ? (meeting.meetees === undefined || meeting.meetees[0] === undefined ? 'https://cdn-images-1.medium.com/max/800/0*9BYGa20RkF4rTpJU.png' : meeting.meetees[0].pictureURL ) : meeting.externals[0].pictureURL}}">
+        <img src="{{ meeting.externals === undefined || meeting.externals[0] === undefined ? (meeting.meetees === undefined || meeting.meetees[0] === undefined || meeting.meetees[0].id === employeeId ? 'https://cdn-images-1.medium.com/max/800/0*9BYGa20RkF4rTpJU.png' : meeting.meetees[0].pictureURL ) : meeting.externals[0].pictureURL}}">
       </span>
     </div>
     <div class="bell" *ngIf="!isPublic">
@@ -37,7 +38,13 @@ export class MeetingComponent implements OnInit{
     @Input() meeting: Meeting;
     @Input('isPublic') isPublic: boolean;
 
+    private employeeId = '';
+
+    constructor(private authenticationService: AuthenticationService) { }
+
     ngOnInit(){
-        console.log(this.isPublic);
+      this.employeeId = this.authenticationService.getId();
+      console.log(this.isPublic);
+      console.log(this.meeting.meetees.length);
     }
 }
